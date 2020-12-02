abf506358b83b598b431ef5356bca7e34ea73bda
SureVX <ben@surevx.com>
2020-12-02 16:04:17 +0100

    feat: completed k8s + Skaffold config

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,23 +5,31 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
     console.log('New post detected. Events emitted');
 
+    events.push(event);
+
     console.log('[EVENTS-BUS] emitting to Posts service');
-    axios.post('http://localhost:4000/events', event);
+    axios.post('http://posts-clusterip-srv:4000/events', event);
 
     console.log('[EVENTS-BUS] emitting to Comments service');
-    axios.post('http://localhost:4001/events', event);
+    axios.post('http://comments-srv:4001/events', event);
 
     console.log('[EVENTS-BUS] emitting to Query service');
-    axios.post('http://localhost:4002/events', event);
+    axios.post('http://query-srv:4002/events', event);
 
     console.log('[EVENTS-BUS] emitting to the Moderation service');
-    axios.post('http://localhost:4003/events', event);
+    axios.post('http://moderation-srv:4003/events', event);
 
     res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 });
 
 app.listen(4005, () => {
